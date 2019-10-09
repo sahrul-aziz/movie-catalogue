1da425b7167f9b07cd6f4683e7ed15325ce955af
Sahrul Abdul Aziz <sahrul.aziz@nttdata.com>
2019-10-09 15:21:07 +0700

    Initial Create Project

---
+++ app/src/main/java/com/dicoding/submission/moviecatalogue/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.submission.moviecatalogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
